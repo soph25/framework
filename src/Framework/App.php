ac6a8c39745579a2a1d228ddc17ed4de1bdbd147
sophie <sophie@sophie-Aspire-X1700>
2018-07-17 22:00:40 +0200

    first commit!

---
+++ src/Framework/App.php
@@ -0,0 +1,26 @@
+<?php
+namespace Framework;
+
+use GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class App
+{
+
+    public function run(ServerRequestInterface $request)
+    {
+        $uri = $request->getUri()->getPath();
+        if (!empty($uri) && $uri[-1] === "/") {
+            $response = new Response();
+            $response = $response->withStatus('301');
+            $response = $response->withHeader('Location', substr($uri, 0, -1));
+            return $response;
+        }
+        if ($uri === '/blog') {
+            return new Response(200, [], '<h1>Bienvenue sur le blog</h1>');
+        }
+    
+        return new Response(404, [], '<h1>Erreur 404 </h1>');
+    }
+}
